71564df48eea9cff95a860c2ae097340baef342d
Soham Mitra <mitra.soham53@gmail.com>
2026-02-13 01:26:18 +0530

    completed function to get other pod IP

--- images/webapp/static/js/script.js
+++ images/webapp/static/js/script.js
@@ -1,6 +1,4 @@
-function getIp() {
-   response = fetch("http://127.0.0.1:8000/getip", {
-    method: "GET",
-
-   }) 
+async function getIp() {
+  // sends get request to IP, awaits response, then parses response into JSON, awaits parsing then prints the fetched Pod IP to alert bar 
+   fetch("http://127.0.0.1:8000/getip").then((response) => response.json()).then((data) => window.alert("Other pod IP is: "+data.message))
 }
