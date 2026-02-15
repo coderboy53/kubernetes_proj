3f2d14349607f23e51c9af8a24e7c3990db90d89
Soham Mitra <mitra.soham53@gmail.com>
2026-02-16 01:48:27 +0530

    added more comments, added path to form, made all the remaining functions async

--- images/webapp/app.py
+++ images/webapp/app.py
@@ -26,13 +26,13 @@ async def form(request: Request):
 
 # path that will get the IP of the current pod and expose it
 @app.get("/ip")
-def selfIp():
+async def selfIp():
     ip = subprocess.run('ip addr show dev eth0 | grep -Ei "inet " | awk \'{print $2}\' | cut -d "/" -f1',shell=True, capture_output=True)
     return {"message": ip.stdout}
 
 # path that will get us the IP of the other pod
 @app.get("/getip")
-def getIP():
+async def getIP():
     color = os.getenv('COLOR')
     if color == 'RED':
         return requests.get("http://blue-webapp.svc.cluster.local/ip")
@@ -41,15 +41,15 @@ def getIP():
 
 # path that will let us store temporary data
 @app.post("/save")
-def saveTemp():
+async def saveTemp():
     return 0
 
 # path that will let us store data in volume
 @app.post("/savev")
-def saveVol():
+async def saveVol():
     return 0
 
 # path that will let us store data in DB
 @app.post("/saved")
-def saveDb():
+async def saveDb():
     return 0
