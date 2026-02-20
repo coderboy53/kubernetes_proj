57e7a6bacc39682fa1aa77c8ec19e80d1e102ab1
Soham Mitra <mitra.soham53@gmail.com>
2026-02-21 02:17:32 +0530

    added code for handling SQL operations

--- images/webapp/app.py
+++ images/webapp/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -6,6 +6,19 @@ import subprocess
 import os
 import requests
 from pydantic import BaseModel
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from typing import Annotated
+
+# model corresponding to table
+class Data(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str | None
+    time: str | None
+    text: str | None
+
+# creating sql connection string and initiating engine
+sql_url = "mysql://"+os.getenv('MYSQL_USER')+":"+os.getenv('MYSQL_PASSWORD')+"@mysql.database.svc.cluster.local/"+os.getenv('MYSQL_DATABASE')
+engine = create_engine(sql_url, echo=True)
 
 app = FastAPI()
 
@@ -15,10 +28,28 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # create a template object
 template = Jinja2Templates(directory="templates")
 
+# model for Form body
 class FormData(BaseModel):
     name: str
     time: str
     text: str
+
+# function that creates the tables as per the SQLModel metadata
+def create_tables():
+    SQLModel.metadata.create_all(engine)
+
+# yields a session 
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+# annotates a new session dependency
+SessionDep = Annotated[Session, Depends(get_session)]
+
+# creates the tables when the app starts up
+@app.on_event("startup")
+def on_startup():
+    SQLModel.metadata.create_all(engine)
 
 # serve the homepage at root
 @app.get("/")
@@ -40,7 +71,7 @@ async def selfIp():
 @app.get("/getip")
 async def getIP():
     color = os.getenv('COLOR')
-    if color == 'RED':
+    if color == 'GREEN':
         return requests.get("http://blue-webapp.svc.cluster.local/ip")
     else:
         return requests.get("http://red-webapp.svc.cluster.local/ip")
@@ -61,5 +92,8 @@ async def saveVol(formData: FormData):
 
 # path that will let us store data in DB
 @app.post("/saved")
-async def saveDb():
-    return 0
+async def saveDb(record: Data, session: SessionDep):
+    session.add(record)
+    session.commit()
+    session.refresh(record)
+    return record
